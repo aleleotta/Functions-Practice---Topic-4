dd7b3c8b621b0e98a5fbd1144e9370f47a51abc5
aless <aless@HPaleleotta>
2022-12-03 18:19:52 +0100

    .

--- Functions Practice - Topic 4/src/exercise3/RandomNumbers.java
+++ Functions Practice - Topic 4/src/exercise3/RandomNumbers.java
@@ -1,4 +1,36 @@
 package exercise3;
 
+import java.util.Scanner;
+
 public class RandomNumbers {
+	static int randomNumbers(int a){
+		int n = 0;
+		Scanner sc = new Scanner(System.in);
+		for(int i = 2; i <= a ; i++) {
+			n = (int)(Math.random() * 2);
+			System.out.print(n + " ");
+		}
+		sc.close();
+		return n;
+	}
+	static int randomNumbers(int b, int c){
+		int n = 0;
+		Scanner sc = new Scanner(System.in);
+		for(int i = 2; i <= b ; i++) {
+			n = (int)(Math.random() * c + 1);
+			System.out.print(n + " ");
+		}
+		sc.close();
+		return n;
+	}
+	static int randomNumbers(int d, int e, int f){
+		int n = 0;
+		Scanner sc = new Scanner(System.in);
+		for(int i = 2; i <= d ; i++) {
+			n = (int)(Math.random() * (f - e + 1) + e);
+			System.out.print(n + " ");
+		}
+		sc.close();
+		return n;
+	}
 }
