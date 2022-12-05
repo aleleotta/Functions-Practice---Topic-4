d223b6095591bcaff48247a2711969e6b40a0e5b
aleleotta <aleleotta@106-02.info2>
2022-12-05 10:10:02 +0100

    .

--- Functions Practice - Topic 4/src/exercise3/Main.java
+++ Functions Practice - Topic 4/src/exercise3/Main.java
@@ -3,7 +3,35 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
+		int option, a, b, c, d, e, f;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Select an option: ");
+		option = sc.nextInt();
+		switch(option) {
+		case 1:
+			System.out.print("Introduce the quantity of digits to print: ");
+			a = sc.nextInt();
+			System.out.println(RandomNumbers.randomNumbers(a));
+			break;
+		case 2:
+			System.out.print("Introduce the quantity of digits to print: ");
+			b = sc.nextInt();
+			System.out.print("Introduce the maximum value: ");
+			c = sc.nextInt();
+			System.out.println(RandomNumbers.randomNumbers(b, c));
+			break;
+		case 3:
+			System.out.print("Introduce the quantity of digits to print: ");
+			d = sc.nextInt();
+			System.out.print("Introduce the minimum value: ");
+			e = sc.nextInt();
+			System.out.print("Introduce the maximum value: ");
+			f = sc.nextInt();
+			System.out.println(RandomNumbers.randomNumbers(d , e, f));
+			break;
+		default:
+			System.out.println("ERROR!");
+		}
 		sc.close();
 	}
 }
