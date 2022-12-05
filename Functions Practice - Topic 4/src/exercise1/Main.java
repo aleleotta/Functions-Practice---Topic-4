aa72e804114521645df9f64ba2a06e66fe1d8167
aless <aless@HPaleleotta>
2022-12-05 16:32:51 +0100

    .

--- Functions Practice - Topic 4/src/exercise1/Main.java
+++ Functions Practice - Topic 4/src/exercise1/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		int option, n;
 		Scanner sc = new Scanner(System.in);
+		Numbers obj1 = new Numbers();
 		System.out.print("Introduce a number: ");
 		n = sc.nextInt();
 		System.out.println("Options: 1) Check if the following number is prime.\n2) Check if the following number has the same number on both first and last digit.");
@@ -12,6 +13,7 @@ public class Main {
 		option = sc.nextInt();
 		switch (option) {
 		case 1:
+			System.out.println("Prime?\n" + obj1.prime(n));
 			break;
 		case 2:
 			break;
